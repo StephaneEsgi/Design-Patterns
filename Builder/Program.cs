3cdacc309f1f8c971368c9fc05402fee62d516db
Stéphane Guardo <stephaneesgi@gmail.com>
2018-04-28 18:23:18 +0200

    [Builder] Ajout du programme d'exécution

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -6,7 +6,24 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var student = Student.Builder()         // initialisation du builder
+                                 .Name("Guardo")    //ajout des différents attributs
+                                 .FirstName("Stéphane")
+                                 .Role("Professeur")
+                                 .Id(3)
+                                 .Build();          // récupération de l'instance garnie de tous les attributs
+
+            System.Console.WriteLine(student);
+
+            // ici on fait une instantiation au travers du contructeur vide, 
+            // puis on ajoute les propriétés une à une à l'instance (méthode "classique")
+            var student2 = new Student();
+            student2.Name = "Guardo";
+            student2.FirstName = "Stéphane";
+            student2.Role = "Professeur";
+            student2.Id = 3;
+
+            System.Console.WriteLine(student2);
         }
     }
 }
