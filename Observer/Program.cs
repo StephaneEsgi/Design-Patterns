34192740a6a17bd2b17a9b22c0cee5d14029e1a5
Stéphane Guardo <stephaneesgi@gmail.com>
2018-04-28 18:43:33 +0200

    [Observer] Ajout du programme d'exécution

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -6,7 +6,16 @@ namespace Observer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // On crée un objet qui hérite d'Observable
+            var observable = new ConcreteObservable();
+
+            // On "inscrit" 2 classes implémentant Observer auprès de l'Observable
+            observable.AddObserver(new ClassA());
+            observable.AddObserver(new ClassB());
+
+            // On déclenche les méthodes Update() au sein des classes inscrites auprès de l'Observable
+            observable.NotifiyObservers();
+
         }
     }
 }
