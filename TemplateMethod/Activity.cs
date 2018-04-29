1000baba70c712660e3e89743d3debad8a3e0dde
Stéphane Guardo <stephaneesgi@gmail.com>
2018-04-29 10:44:08 +0200

    [TemplateMethod] Ajout d'une classe abstraite Activity

---
+++ TemplateMethod/Activity.cs
@@ -0,0 +1,56 @@
+using System;
+using static System.Console;
+
+namespace TemplateMethod
+{
+    public abstract class Activity
+    {
+        public void Begin()
+        {
+            OnCreate();
+            OnStart();
+            OnResume();
+        }
+
+        public void End()
+        {
+            OnPause();
+            OnStop();
+            OnDestroy();
+        }
+
+        public virtual void OnCreate()
+        {
+            WriteLine("[Activity] OnCreate()");
+            WriteLine($"[Activity] Instatiation du layout '{GetLayoutRes()}'");
+        }
+
+        public virtual void OnStart()
+        {
+            WriteLine("[Activity] OnStart()");
+        }
+
+        public virtual void OnResume()
+        {
+            WriteLine("[Activity] OnResume()");
+        }
+
+        public virtual void OnPause()
+        {
+            WriteLine("[Activity] OnPause()");
+        }
+
+        public virtual void OnStop()
+        {
+            WriteLine("[Activity] OnStop()");
+        }
+
+        public virtual void OnDestroy()
+        {
+            WriteLine("[Activity] OnDestroy()");
+        }
+
+        public abstract string GetLayoutRes();
+
+    }
+}
