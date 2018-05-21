1a9a2b085fa49bffa7828f63aafe04ae22334267
Stéphane Guardo <stephaneesgi@gmail.com>
2018-05-21 23:00:02 +0200

    [AbstractFactory] Création des modèles

---
+++ AbstractFactory/Models/Cpu.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbstractFactory 
+{
+    public class Cpu 
+    {
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ AbstractFactory/Models/Ram.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbstractFactory 
+{
+    public class Ram 
+    {
+        public string RamType { get; set; }
+        public int GbSize { get; set; }
+        public int Price { get; set; }
+    }
+}
